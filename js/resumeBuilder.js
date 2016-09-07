2af0540f0c37b3db5dffbc8e8fe94787a68d8d20
Paulo Fernando <nandooferraz@gmail.com>
2016-09-07 20:27:17 -0300

    Created work resume section"

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,6 +6,7 @@ $(function () {
 
     function init() {
         bio.display();
+        work.display();
     }
 
     function getBio() {
@@ -20,7 +21,7 @@ $(function () {
                 location: 'New York City'
             },
             welcomeMessage: 'Shut up and pay me money',
-            skills: ['Savior of the universe', 'Amazing Deliverboy', 'Awesome Pilot', 'Destroyer of the universe'],
+            skills: ['Savior of the universe', 'Amazing Deliver boy', 'Awesome Pilot', 'Destroyer of the universe'],
             biopic: '../images/fry.jpg',
             display: function () {
                 var header = $('#header');
@@ -48,14 +49,30 @@ $(function () {
     function getWork() {
         return {
             jobs: [{
-                employer: '',
-                title: '',
-                location: '',
-                dates: '',
-                description: '',
+                employer: 'Planet Express',
+                title: 'Delivery Boy',
+                location: 'Brooklyns, NY',
+                dates: 'January 3000 - In progress',
+                description: 'Cheese on toast the big cheese camembert de normandie. Ricotta caerphilly queso stinking bishop danish fontina cheddar bavarian bergkase melted cheese. Camembert de normandie queso taleggio airedale cheesy feet goat cheese and wine cheeseburger. Taleggio pecorino swiss taleggio gouda cheddar.',
+            }, {
+                employer: 'Panucci\'s Pizza',
+                title: 'Delivery Boy',
+                location: 'Manhattan, NY',
+                dates: '1998 - Decembe 31, 1999',
+                description: 'Cheddar cheese and wine gouda. Swiss hard cheese halloumi cheesecake when the cheese comes out everybody\'s happy paneer boursin cheeseburger. Monterey jack bocconcini bocconcini bocconcini babybel hard cheese edam cheese and wine. Cheese slices mozzarella.',
             }],
             display: function () {
-                
+                $('#workExperience').append(HTMLworkStart);
+
+                this.jobs.forEach(function(job) {
+                    var workEntry = $('.work-entry');
+
+                    var employerTitle = fillData(HTMLworkEmployer, job.employer) + fillData(HTMLworkTitle, job.title);
+                    workEntry.append(employerTitle);
+                    workEntry.append(fillData(HTMLworkDates, job.dates));
+                    workEntry.append(fillData(HTMLworkLocation, job.location));
+                    workEntry.append(fillData(HTMLworkDescription, job.description));
+                });
             }
         };
     }
